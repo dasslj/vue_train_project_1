544c0fd4c3c05ac5d959fa7757154ccaa6e253c8
dasslj <dasslj@163.com>
2024-08-05 17:35:57 +0800

    第一天

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,31 +1,57 @@
-
-
 <template>
-    <div>
-  
-      <div class="mainWindow">
-          <h2>登录</h2>
-          
-      </div>
-  
+  <div>
+    <div class="mainWindow">
+      <el-col class="mainCol">
+        <h2>登录</h2>
+        <el-row>
+          <span>用户名/邮箱：</span>
+          <el-input></el-input>
+        </el-row>
+
+        <el-row>
+          <span>密码：</span>
+          <el-input></el-input>
+        </el-row>
+      </el-col>
+      <el-row class="mainRow">
+        <el-button>登录</el-button>
+        <span style="width: 159px"></span>
+        <el-button>取消</el-button>
+      </el-row>
+
+      <router-link
+      to="/login"
+      >
+        没有账号？来注册吧
+      </router-link>
     </div>
-  </template>
+  </div>
+</template>
   
-  <script setup>
+<script setup>
+// import {reactive, ref} from "vue"
+
+
+
+</script>
   
-  </script>
-  
-  <style scoped>
-  
-  
-  .mainWindow{
-      width: 500px;
-      height: 400px;
-      /* border: 1px red solid; */
-      margin: 300px auto;
-      border-radius: 7px;
-      box-shadow: black 0px 0px 10px;
-  
-  }
-  
-  </style>
+<style scoped>
+.mainRow {
+  width: 300px;
+  margin: 10px auto;
+}
+
+.mainCol {
+  height: 300px;
+  width: 300px;
+  margin: 10px auto;
+}
+
+.mainWindow {
+  width: 500px;
+  height: 400px;
+  margin: 300px auto;
+  border-radius: 7px;
+  box-shadow: black 0px 0px 10px;
+}
+</style>
